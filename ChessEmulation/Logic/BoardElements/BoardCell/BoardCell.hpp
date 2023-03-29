e10dd841cd0cc2460e7092632a3c8aacd92ca7de
Alexander Chernenko <letmeexist95@gmail.com>
2023-03-29 18:36:36 +0300

    Reorganized the file{

--- ChessEmulation/Logic/BoardElements/BoardCell/BoardCell.hpp
+++ ChessEmulation/Logic/BoardElements/BoardCell/BoardCell.hpp
@@ -16,15 +16,15 @@ public:
 	BoardCell(const std::pair<uint8_t, char> index, std::shared_ptr<sf::Texture> texture,
 		const std::shared_ptr<Piece> piece);
 
+	~BoardCell() = default;
+
 	const inline std::pair<uint8_t, char>& GetIndex() const noexcept { return m_Index; }
-	
+
 	const inline std::shared_ptr<Piece>& GetPiece() const noexcept { return m_Piece; }
 	void SetPiece(const std::shared_ptr<Piece> piece) noexcept;
-	
-	void FreeCell() noexcept;
 
 	inline bool IsFree() const noexcept { return static_cast<bool>(m_Piece); }
-
+	void FreeCell() noexcept;
 
  	BoardCell& operator = (const BoardCell&) = delete;
 	BoardCell& operator = (BoardCell&&) = delete;
