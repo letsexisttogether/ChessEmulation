329d78c63fd3e05b409f6b1504ac4b8801fc5526
Alexsander Chernenko <letmeexist95@gmail.com>
2023-11-08 22:13:19 +0200

    Added a method for checking the move possibility

--- Source/Logic/PieceElements/Piece/Piece.cpp
+++ Source/Logic/PieceElements/Piece/Piece.cpp
@@ -1,8 +1,27 @@
 #include "Piece.hpp"
 
-Piece::Piece(const sf::Texture& texture, Side side)
+Piece::Piece(const sf::Texture& texture, const Side side)
 	: m_Sprite{ texture }, m_Side{ side }
 {}
+
+Piece::Piece(const Side side, const MovesContainer& moves)
+    : m_Side{ side }, m_Moves{ moves }
+{}
+
+MoveEffect Piece::TryMove(const BoardCell& initial, const BoardCell& final) 
+    const noexcept
+{
+    for (const MovePointer& move : m_Moves)
+    {
+        if (const MoveEffect effect = move->CheckRequirements(initial, final);
+                effect != MoveEffect::NONE)
+        {
+            return effect;
+        }
+    }
+
+    return MoveEffect::NONE;
+}
 
 void Piece::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
--- Source/Logic/PieceElements/Piece/Piece.hpp
+++ Source/Logic/PieceElements/Piece/Piece.hpp
@@ -1,25 +1,31 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <memory>
 #include <vector>
 
 #include "Side.hpp"
-#include "Logic/PieceElements/Move/PieceMove.hpp"
-
-class BoardCell;
+#include "Logic/PieceElements/Move/BasicMove.hpp"
 
 class Piece : public sf::Drawable
 {
+public:
+    using MovePointer = BasicMove*;
+    using MovesContainer = std::vector<MovePointer>;
+
 public:
 	Piece() = delete;
 	Piece(const Piece&) = delete;
 	Piece(Piece&&) = delete;
 
-	Piece(const sf::Texture& texture, Side side);
+	Piece(const sf::Texture& texture, const Side side);
+    Piece(const Side side, const MovesContainer& moves);
 
 	virtual ~Piece() = default;
 
     // MoveResult
+    MoveEffect TryMove(const BoardCell& initial, 
+            const BoardCell& final) const noexcept;
 
 	inline const sf::Vector2f& GetScreenPosition() const noexcept { return m_Sprite.getPosition(); }
 	void SetScreenPosition(const sf::Vector2f& position) noexcept;
@@ -35,8 +41,11 @@ public:
 protected:
 	Side m_Side{};
 	sf::Sprite m_Sprite{};
-    std::vector<PieceMove> m_Moves;
+    MovesContainer m_Moves{};
 
+
+
+    // Old ideas, let's just leave them as that
 
 	// Includes default moves and attacks
 	// std::vector<PieceMove> m_AllPossibleMoves{};
