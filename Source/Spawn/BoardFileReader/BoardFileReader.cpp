0a46a57c9fae7f6731e9364fc9a7d5eef32c4cc1
Александр Черненко <letsexisttogether>
2024-01-11 20:53:09 +0200

    Added first (UNTESTED) version of BoardFileReader

---
+++ Source/Spawn/BoardFileReader/BoardFileReader.cpp
@@ -0,0 +1,141 @@
+#include "BoardFileReader.hpp"
+
+#include <SFML/Graphics.hpp>
+#include <exception>
+
+BoardFileReader::BoardFileReader(const std::string& fileName, 
+        const Piece::InMovesContainer& moves):
+    m_FileStream{ fileName }, m_Moves{ moves }
+{}
+
+Board BoardFileReader::GetBoard() noexcept(false)
+{
+    m_FileStream.seekg(std::streamsize(0), std::ios::beg);
+
+    Board board{};
+
+    CheckSignature();
+    
+    for (std::string line; m_FileStream >> line; )
+    {
+        if (line.empty())
+        {
+            continue;
+        }
+
+        const std::string lineSubstr{ line.substr(3) };
+        std::istringstream lineRef{ lineSubstr };
+
+        switch (char actionIdentifier = line[0])
+        {
+            case 't':
+                LoadTexture(lineSubstr);
+                break;
+            case 'p':
+                CreatePiece(lineRef);
+                break;
+            case 'c':
+                CreateCell(board, lineRef);
+                break;
+            case 'b':
+                SetBoardTexture(board, lineRef);
+                break;
+
+            default:
+                throw std::exception{ "Unknown letter was found in the file" };
+        }
+    }
+
+
+    return board;
+}
+
+void BoardFileReader::LoadTexture(const std::string& filePath) 
+    noexcept(false)
+{
+    sf::Texture* texture = new sf::Texture{};
+    texture->loadFromFile(filePath);
+
+    m_Textures.push_back(texture);
+}
+
+void BoardFileReader::CreatePiece(std::istringstream& pieceArguments) 
+    noexcept(false)
+{
+    std::int32_t pieceSide{};
+    std::int32_t pieceType{};
+    std::size_t textureIndex{}; 
+
+    pieceArguments >> pieceSide >> pieceType >> textureIndex;
+
+    Piece::InMovesContainer moves{};
+
+    for (std::size_t moveIndex; !pieceArguments.eof(); )
+    {
+        pieceArguments >> moveIndex;
+
+        moves.push_back(m_Moves[moveIndex]);
+    }
+
+    m_Pieces.push_back(new Piece
+            {
+                static_cast<PieceSide>(pieceSide),
+                static_cast<PieceType>(pieceType),
+                m_Textures[textureIndex],
+                moves
+            });
+}
+
+void BoardFileReader::CreateCell(Board& board, std::istringstream& cellAruments) 
+    noexcept(false)
+{
+    BoardCellIndex::Rank rank{};
+    BoardCellIndex::File file{};
+    std::size_t textureIndex{};
+
+    cellAruments >> rank >> file >> textureIndex;
+
+    BoardCell cell{ BoardCellIndex{ rank, file }, m_Textures[textureIndex] };
+
+    if (!cellAruments.eof())
+    {
+        std::size_t pieceIndex;
+
+        cellAruments >> pieceIndex;
+
+        cell.SetPiece(m_Pieces[pieceIndex]);
+    }
+
+    board.AddCell(std::move(cell));
+}
+
+void BoardFileReader::SetBoardTexture(Board& board, 
+        std::istringstream& boardArguments)
+    noexcept(false)
+{
+    std::size_t textureIndex{};
+    
+    boardArguments >> textureIndex;
+
+    board.SetTexture(m_Textures[textureIndex]);
+}
+
+void BoardFileReader::CheckSignature() noexcept(false)
+{
+    std::string signature{};
+    m_FileStream >> signature;
+
+
+    if (signature != "BRDCA")
+    {
+        throw std::exception{ "The file is missing the signature" };
+    }
+}
+
+void BoardFileReader::CleanUp() noexcept
+{
+    m_FileStream.close();
+
+    m_Textures.clear();
+    m_Pieces.clear();
+}
---
+++ Source/Spawn/BoardFileReader/BoardFileReader.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <fstream>
+#include <sstream>
+
+#include "Logic/Board/Board.hpp"
+#include "Logic/Board/Cell/BoardCell.hpp"
+
+class BoardFileReader
+{
+public:
+    BoardFileReader() = delete;
+    BoardFileReader(const BoardFileReader&) = delete;
+    BoardFileReader(BoardFileReader&&) = delete;
+
+    BoardFileReader(const std::string& fileName, 
+            const Piece::InMovesContainer& moves);
+
+    ~BoardFileReader() = default;
+
+    Board GetBoard() noexcept(false);
+
+    BoardFileReader& operator = (const BoardFileReader&) = delete;
+    BoardFileReader& operator = (BoardFileReader&&) = delete;
+
+private:
+    void LoadTexture(const std::string& filePath) 
+        noexcept(false);
+    void CreatePiece(std::istringstream& pieceArguments) 
+        noexcept(false);
+    void CreateCell(Board& board, std::istringstream& cellAruments) 
+        noexcept(false);
+    void SetBoardTexture(Board& board, std::istringstream& boardArguments)
+        noexcept(false);
+
+    void CheckSignature() noexcept(false);
+
+    void CleanUp() noexcept;
+
+private:
+    std::ifstream m_FileStream;
+
+    Piece::InMovesContainer m_Moves;
+
+    std::vector<sf::Texture*> m_Textures{};
+    std::vector<Piece*> m_Pieces{};
+};
