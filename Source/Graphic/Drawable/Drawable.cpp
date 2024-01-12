48a724c40fd4b1866ab755363e829077f46edba9
Александр Черненко <letsexisttogether>
2024-01-12 17:52:47 +0200

    Added default position for Drawable

--- Source/Graphic/Drawable/Drawable.cpp
+++ Source/Graphic/Drawable/Drawable.cpp
@@ -1,17 +1,32 @@
 #include "Drawable.hpp"
-#include <utility>
 
-Drawable::Drawable(sf::Texture* const texture)
-    : m_Texture{ texture }
-{}
+#include <SFML/Graphics/PrimitiveType.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/VertexArray.hpp>
+#include <stdexcept>
+
+Drawable::Drawable(sf::Texture* const texture, 
+        const Position& position)
+{
+    SetTexture(texture);
+
+    setPosition(position);
+}
 
 Drawable::Drawable(const Drawable& drawable)
-    : m_Texture{ drawable.m_Texture }
-{}
+{
+    SetTexture(drawable.m_Texture.get());
+
+    setPosition(drawable.getPosition());
+}
 
 Drawable::Drawable(Drawable&& drawable)
-    : m_Texture{ std::move(drawable.m_Texture) }
-{}
+{
+    SetTexture(drawable.m_Texture.get());
+    drawable.m_Texture.reset();
+
+    setPosition(drawable.getPosition());
+}
 
 const sf::Texture& Drawable::GetTexture() const noexcept(false)
 {
@@ -20,30 +35,51 @@ const sf::Texture& Drawable::GetTexture() const noexcept(false)
     return *m_Texture;
 }
 
-void Drawable::SetTexture(sf::Texture* const texture) noexcept
+void Drawable::SetTexture(sf::Texture* const texture) noexcept(false)
 {
     m_Texture.reset(texture);
+
+    CheckTexture();
 }
 
 void Drawable::draw(sf::RenderTarget& target, sf::RenderStates states) 
     const
 {
-    CheckTexture();
+    states.transform *= getTransform();
+    states.texture = m_Texture.get();
+    
+    // Create a quad (two triangles) for rendering the texture
+    sf::VertexArray vertices(sf::Quads, 4);
+    vertices[0].position = sf::Vector2f(0, 0);
+    vertices[1].position = sf::Vector2f(m_Texture->getSize().x, 0);
+    vertices[2].position = sf::Vector2f(m_Texture->getSize().x, m_Texture->getSize().y);
+    vertices[3].position = sf::Vector2f(0, m_Texture->getSize().y);
 
-    sf::Sprite sprite(*m_Texture);
-    target.draw(sprite, states);
+    // Set texture coordinates
+    vertices[0].texCoords = sf::Vector2f(0, 0);
+    vertices[1].texCoords = sf::Vector2f(m_Texture->getSize().x, 0);
+    vertices[2].texCoords = sf::Vector2f(m_Texture->getSize().x, m_Texture->getSize().y);
+    vertices[3].texCoords = sf::Vector2f(0, m_Texture->getSize().y);
+
+    // Apply the transform and draw the vertices
+    target.draw(vertices, states);
 }
 
-Drawable& Drawable::operator = (const Drawable& drawable)
+Drawable& Drawable::operator = (const Drawable& drawable) noexcept(false)
 {
-    m_Texture = drawable.m_Texture;
+    SetTexture(drawable.m_Texture.get());
+
+    setPosition(drawable.getPosition());
 
     return *this;
 }
 
-Drawable& Drawable::operator = (Drawable&& drawable)
+Drawable& Drawable::operator = (Drawable&& drawable) noexcept(false)
 {
-    m_Texture = std::move(drawable.m_Texture);
+    SetTexture(drawable.m_Texture.get());
+    drawable.m_Texture.reset();
+
+    setPosition(drawable.getPosition());
 
     return *this;
 }
@@ -54,4 +90,6 @@ void Drawable::CheckTexture() const noexcept(false)
     {
         throw std::runtime_error{ "The texure is nullptr" };
     }
-}
+} 
+
+const Drawable::Position Drawable::DefaultPosition{ 0, 0 };
--- Source/Graphic/Drawable/Drawable.hpp
+++ Source/Graphic/Drawable/Drawable.hpp
@@ -1,27 +1,38 @@
-#include <SFML/Graphics.hpp>
+#include <memory>
 
-class Drawable: public sf::Drawable
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/Transformable.hpp>
+
+class Drawable: public sf::Drawable, public sf::Transformable
 {
+public:
+    using Position = sf::Vector2f;
+
 public:
     Drawable() = default;
     Drawable(const Drawable& drawable);
     Drawable(Drawable&& drawable);
-    
-    Drawable(sf::Texture* const texture);
+
+    Drawable(sf::Texture* const texture, 
+            const Position& position = DefaultPosition);
 
     virtual ~Drawable() = default;
 
     const sf::Texture& GetTexture() const noexcept(false);
-    void SetTexture(sf::Texture* const texture) noexcept;
+    void SetTexture(sf::Texture* const texture) noexcept(false);
 
     void draw(sf::RenderTarget& target, sf::RenderStates states) 
         const override;
 
-    Drawable& operator = (const Drawable& drawable);
-    Drawable& operator = (Drawable&& drawable);
+    Drawable& operator = (const Drawable& drawable) noexcept(false);
+    Drawable& operator = (Drawable&& drawable) noexcept(false);
 
 private:
     void CheckTexture() const noexcept(false);
+
+public:
+    static const Position DefaultPosition;
 
 protected:
     std::shared_ptr<sf::Texture> m_Texture{};
