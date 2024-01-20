91c3a4bbde5ce999b06d4f8df72401896e7a514b
letsexisttogether <letmeexist95@gmail.com>
2024-01-21 00:12:20 +0200

    Implemented Match

---
+++ Source/Logic/Match/Match.cpp
@@ -0,0 +1,46 @@
+#include "Match.hpp"
+
+#include <utility>
+
+Match::Match(Match&& match)
+    : m_Board{ std::move(match.m_Board) }, 
+    m_GameObserver{ match.m_GameObserver },
+    m_TurnSide{ match.m_TurnSide },
+    m_TurnCount{ match.m_TurnCount }
+{}
+
+Match::Match(Board&& board)
+    : m_Board{ std::move(board) }
+{}
+ 
+Board& Match::GetBoard() noexcept
+{
+    return m_Board;
+}
+
+const Board& Match::GetBoard() const noexcept
+{
+    return m_Board;
+}
+
+GameObserver& Match::GetGameObserver() noexcept
+{
+    return m_GameObserver;
+}
+
+const GameObserver& Match::GetGameObserver() const noexcept
+{
+    return m_GameObserver;
+}
+
+const PieceSide Match::GetTurnSide() const noexcept
+{
+    return m_TurnSide.GetSide();
+}
+
+
+const Match::TurnCount Match::GetTurnCount() 
+    const noexcept
+{
+    return m_TurnCount;
+}
--- Source/Logic/Match/Match.hpp
+++ Source/Logic/Match/Match.hpp
@@ -4,25 +4,42 @@
 
 #include "Control/Controller.hpp"
 #include "Logic/Board/Board.hpp"
+#include "Logic/Gameplay/GameObserver/GameObserver.hpp"
+#include "Logic/Gameplay/SideHolder/PieceSideHolder.hpp"
 
+// Representation of a default match
 class Match
 {
 public:
-    using ControllerPointer = std::shared_ptr<Controller>;
+    using TurnCount = std::uint16_t;
 
 public:
     Match() = delete;
     Match(const Match&) = delete;
-    Match(Match&&) = delete;
+    Match(Match&& match);
 
-    // C_tor();
+    Match(Board&& board);
 
     ~Match() = default;
 
-private:
-    Board m_Board{};  
-    ControllerPointer m_Controller{};    
-    Side m_CurrentSide{};
+    Board& GetBoard() noexcept;
+    const Board& GetBoard() const noexcept;
 
-    // const Timer m_Timer;
+    GameObserver& GetGameObserver() noexcept;
+    const GameObserver& GetGameObserver() const noexcept;
+
+    // I hope me from the future will get more experienced 
+    // and now can deal with returning const or non-const 
+    // if we return an easy-copyable field
+    const PieceSide GetTurnSide() const noexcept;
+
+    const TurnCount GetTurnCount() const noexcept;
+
+private:
+    Board m_Board;
+    GameObserver m_GameObserver{};
+
+    PieceSideHolder m_TurnSide{};   
+    TurnCount m_TurnCount{};
+    
 };
